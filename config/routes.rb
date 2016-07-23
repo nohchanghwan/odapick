f0ff19d90cacbb7e6f2db9107803cbc8c881e3ce
노창환 <n_jia@naver.com>
2016-07-23 16:21:07 +0900

    [CRUD ] C 부분개발및 테스트(title,content) 디비사용전 드롭한번하고 다시 migrate 시켜서 확인하면댑니다.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'index#index'
+
+   get 'index/index'
+   post 'index/create'
+   get 'index/new'
+   get 'index/edit'
+   get 'index/distroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
