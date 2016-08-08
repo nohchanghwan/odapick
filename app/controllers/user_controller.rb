2255f5d4ff0007415633e1edd3eca589fe179724
윤소연 <sssoyeon92@naver.com>
2016-08-08 15:25:44 +0900

    css, fonts, js 폴더 assets으로 이동 / join_form 수정중

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,20 @@
 class UserController < ApplicationController
 
+  def join_form
+
+  end
+
   def join
 
   end
 
-  def getList
+  #유저 전체의 정보 가져오는 메소드
+  def get_list
+
+  end
+
+  #유저 한명의 정보 가져오는 메소드
+  def get_user
 
   end
 
